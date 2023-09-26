207b1e3483b337f980b9091e79db5fcd1edca3d5
sanadbawab0 <sanadbawab0@gmail.com>
2023-09-26 14:36:39 +0300

    9/26/2023

---
+++ base/urls/order_urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from base.views.order_views import *
+
+
+urlpatterns = [
+    
+]
---
+++ base/urls/product_urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from base.views.product_views import *
+
+
+urlpatterns = [
+    path('', getProducts, name="products"),
+    path('<str:pk>/', getProduct, name="product"),
+]
